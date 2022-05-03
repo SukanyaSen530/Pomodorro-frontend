027e2f7585669a586e79c140d5cf31f901326731
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-05-03 12:45:33 +0530

    feat- added transition to task form

--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 import "./tasks.scss";
 
-import { TaskForm, TaskCard } from "../../components";
+import { TaskForm } from "../../components";
 
 const initialState = {
   title: "",
@@ -14,9 +15,14 @@ const initialState = {
 
 const Tasks = () => {
   const [showForm, setShowForm] = useState(false);
-  const handleForm = () => setShowForm((val) => !val);
+
+  const handleClose = () => {
+    setShowForm((val) => !val);
+    setTaskData({ ...initialState });
+  };
 
   const [taskData, setTaskData] = useState({ ...initialState });
+
   const handleChange = ({ target: { name, value } }) => {
     setTaskData((prevVal) => ({ ...prevVal, [name]: value }));
     console.log(taskData);
@@ -40,20 +46,30 @@ const Tasks = () => {
       <div className="tasks-section__content">
         <div className="flex flex-space-between">
           <h4 className="h4">Todo-List</h4>
-          <button className="btn-float defaultDark" onClick={handleForm}>
+          <button
+            className="btn-float defaultDark"
+            onClick={() => setShowForm(true)}
+          >
             +
           </button>
         </div>
         <div className="tasks-section__content__details"></div>
       </div>
 
-      <TaskForm
-        open={showForm}
-        onClose={handleForm}
-        taskData={taskData}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-      />
+      <CSSTransition
+        in={showForm}
+        timeout={300}
+        classNames="modal"
+        unmountOnExit
+      >
+        <TaskForm
+          open={showForm}
+          onClose={handleClose}
+          taskData={taskData}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      </CSSTransition>
     </section>
   );
 };
