b6adb316f8a794af21db3b050593a32776ea074f
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-05-04 11:21:23 +0530

    fix - updated the mode

--- src/services/url.js
+++ src/services/url.js
@@ -1,7 +1,6 @@
 const { REACT_APP_APP_MODE, REACT_APP_BASE_URL, REACT_APP_DEV_URL } =
   process.env;
-const api =
-  REACT_APP_APP_MODE === "dev" ? REACT_APP_DEV_URL : REACT_APP_BASE_URL;
+const api = REACT_APP_APP_MODE === "" ? REACT_APP_DEV_URL : REACT_APP_BASE_URL;
 
 const tasksURL = `${api}tasks`;
 const authURL = `${api}auth/`;
