e85b8e7a465b691b54e25b9a962941ad583f5efc
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-05-06 10:10:07 +0530

    fix - loading tasks issue

--- src/context/providers/TaskProvider.js
+++ src/context/providers/TaskProvider.js
@@ -1,5 +1,6 @@
 import { useEffect, useReducer, useContext, createContext } from "react";
 import taskReducer from "../reducers/taskReducer";
+import { useAuthContext } from "../";
 
 import { getTasks } from "../../services";
 
@@ -14,10 +15,16 @@ const initialState = {
 const TaskProvider = ({ children }) => {
   const [state, dispatch] = useReducer(taskReducer, initialState);
 
+  const {
+    authState: {
+      user: { token },
+    },
+  } = useAuthContext();
+
   useEffect(() => {
-    if (state.tasks?.length === 0) getTasks(dispatch);
+    if (state.tasks?.length === 0 && token) getTasks(dispatch);
     // eslint-disable-next-line
-  }, [dispatch]);
+  }, [dispatch, token]);
 
   return (
     <taskContext.Provider
